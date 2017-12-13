bc8336aabcdc7ac40e152f67f92b40b1cf631821
Danny Kendrick <dkendrick@inspiregroup.co.nz>
2017-12-13 14:47:54 +1300

    Adding knowledgebase

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 import * as builder from 'botbuilder';
+import * as cognitiveservices from 'botbuilder-cognitiveservices';
 import restify from 'restify';
+
+// https://docs.moodle.org/24/en/About_Moodle_FAQ
 
 // Setup Restify Server
 const server = restify.createServer();
@@ -20,6 +23,17 @@ server.get('/', (req, res, cb) => {
 
 server.post('/api/messages', connector.listen());
 
-const bot = new builder.UniversalBot(connector, (session) => {
-  session.send(`${session.message.text}%s?`);
+const bot = new builder.UniversalBot(connector);
+
+const recognizer = new cognitiveservices.QnAMakerRecognizer({
+  knowledgeBaseId: process.env.KnowledgeBaseId,
+  subscriptionKey: process.env.KnowledgeBaseKey,
 });
+
+const basicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
+  recognizers: [recognizer],
+  defaultMessage: 'No match! Try changing the query terms!',
+  qnaThreshold: 0.3,
+});
+
+bot.dialog('/', basicQnAMakerDialog);
